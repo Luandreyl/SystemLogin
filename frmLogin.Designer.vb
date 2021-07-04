58646dd12e518a69583f68c4b013f6fb4e7e2cb6
Luandreyl <54290277+Luandreyl@users.noreply.github.com>
2021-07-04 01:33:26 -0300

    Add files via upload

---
+++ frmLogin.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLogin
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmLogin))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtUser = New System.Windows.Forms.TextBox()
+        Me.txtPass = New System.Windows.Forms.TextBox()
+        Me.btnLogar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MV Boli", 33.0!, System.Drawing.FontStyle.Bold)
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(74, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(263, 58)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Bem vindo!"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 111)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(54, 21)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "User:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 150)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(94, 21)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Password:"
+        '
+        'txtUser
+        '
+        Me.txtUser.Font = New System.Drawing.Font("MV Boli", 8.0!, System.Drawing.FontStyle.Bold)
+        Me.txtUser.Location = New System.Drawing.Point(112, 112)
+        Me.txtUser.Name = "txtUser"
+        Me.txtUser.Size = New System.Drawing.Size(225, 25)
+        Me.txtUser.TabIndex = 3
+        '
+        'txtPass
+        '
+        Me.txtPass.Font = New System.Drawing.Font("MV Boli", 8.0!, System.Drawing.FontStyle.Bold)
+        Me.txtPass.Location = New System.Drawing.Point(112, 151)
+        Me.txtPass.Name = "txtPass"
+        Me.txtPass.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPass.Size = New System.Drawing.Size(180, 25)
+        Me.txtPass.TabIndex = 4
+        '
+        'btnLogar
+        '
+        Me.btnLogar.Location = New System.Drawing.Point(142, 198)
+        Me.btnLogar.Name = "btnLogar"
+        Me.btnLogar.Size = New System.Drawing.Size(126, 39)
+        Me.btnLogar.TabIndex = 5
+        Me.btnLogar.Text = "Logar"
+        Me.btnLogar.UseVisualStyleBackColor = True
+        '
+        'frmLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 21.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.ClientSize = New System.Drawing.Size(411, 249)
+        Me.Controls.Add(Me.btnLogar)
+        Me.Controls.Add(Me.txtPass)
+        Me.Controls.Add(Me.txtUser)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("MV Boli", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(5, 5, 5, 5)
+        Me.MaximizeBox = False
+        Me.Name = "frmLogin"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtUser As TextBox
+    Friend WithEvents txtPass As TextBox
+    Friend WithEvents btnLogar As Button
+End Class
---
+++ frmLogin.vb
@@ -0,0 +1,19 @@
+﻿Public Class frmLogin
+    Public Sub frmLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Public Sub btnLogar_Click(sender As Object, e As EventArgs) Handles btnLogar.Click
+
+        Dim user As String = "admin" ' Variavel do usuario
+        Dim pass As String = "admin" ' Variavel da senha
+
+        If (txtUser.Text & txtPass.Text = user & pass) Then ' Faz a verificação se os dados informados estão corretos
+
+            MessageBox.Show("Login correto!", "Logado com Sucesso") ' Exibe mensagem avisando sobre sucesso ao fazer Login
+        Else
+            MessageBox.Show("Usuário ou senha está incorreto!", "Erro") 'Exibe mensagem sobre erro ao tentar fazer login com informações incorretas
+
+        End If
+    End Sub
+End Class
